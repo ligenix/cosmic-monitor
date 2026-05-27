172c6dd78e0c3c50000125440c03deb910c69a05
Jeremy Soller <jackpot51@gmail.com>
2026-05-26 21:14:39 -0600

    linux-fdinfo: support multiple PIDs and human format data

--- examples/linux-fdinfo.rs
+++ examples/linux-fdinfo.rs
@@ -1,29 +1,40 @@
-use std::{fs, path::Path, time::Instant};
+use std::{
+    fs,
+    path::Path,
+    time::{Duration, Instant},
+};
 
 fn main() {
-    let pid = std::env::args().nth(1).unwrap();
-    let instant = Instant::now();
-    let proc_path = Path::new("/proc").join(&pid);
-    let proc_fd_path = proc_path.join("fd");
-    let proc_fdinfo_path = proc_path.join("fdinfo");
     // Do we have to collect more than one per process?
     let mut fdinfos = Vec::new();
-    for entry_res in fs::read_dir(&proc_fd_path).unwrap() {
-        let Ok(entry) = entry_res else { continue };
-        let path = entry.path();
-        let Ok(link) = fs::read_link(&path) else {
+
+    let instant = Instant::now();
+    for pid in std::env::args().skip(1) {
+        let proc_path = Path::new("/proc").join(&pid);
+        let proc_fd_path = proc_path.join("fd");
+        let proc_fdinfo_path = proc_path.join("fdinfo");
+        let Ok(entries) = fs::read_dir(&proc_fd_path) else {
             continue;
         };
-        if link.starts_with("/dev/dri") {
-            let name = entry.file_name();
-            if let Ok(data) = fs::read_to_string(proc_fdinfo_path.join(&name)) {
-                fdinfos.push((name, link, data));
+        for entry_res in entries {
+            let Ok(entry) = entry_res else { continue };
+            let path = entry.path();
+            let Ok(link) = fs::read_link(&path) else {
+                continue;
+            };
+            if link.starts_with("/dev/dri") {
+                let name = entry.file_name();
+                if let Ok(data) = fs::read_to_string(proc_fdinfo_path.join(&name)) {
+                    fdinfos.push((pid.clone(), name, link, data));
+                }
             }
         }
     }
     let elapsed = instant.elapsed();
-    for (name, link, data) in fdinfos {
-        println!("{}: {}", name.display(), link.display());
+
+    let instant = Instant::now();
+    for (pid, name, link, data) in fdinfos {
+        println!("{}: {}: {}", pid, name.display(), link.display());
         for line in data.lines() {
             let Some((key, value)) = line.split_once(":") else {
                 continue;
@@ -32,22 +43,56 @@ fn main() {
             if let Some(key) = key.strip_prefix("drm-") {
                 let value = value.trim_start();
                 if let Some(key) = key.strip_prefix("total-") {
-                    let Some((value, suffix)) = value.split_once(' ') else {
+                    let mut parts = value.splitn(2, ' ');
+                    let Ok(mut bytes) = parts.next().unwrap_or_default().parse::<u64>() else {
                         continue;
                     };
-                    println!("  total {}: {} {}", key, value, suffix);
+                    match parts.next().unwrap_or_default() {
+                        "KiB" => {
+                            // Kilobytes
+                            bytes *= 1024;
+                        }
+                        "MiB" => {
+                            // Megabytes
+                            bytes *= 1024 * 1024;
+                        }
+                        // Other suffixes not defined
+                        _ => {
+                            continue;
+                        }
+                    }
+                    println!(
+                        "  total {}: {}",
+                        key,
+                        humansize::format_size(bytes, humansize::BINARY)
+                    );
                 }
                 if let Some(key) = key.strip_prefix("engine-") {
                     if key.starts_with("capacity-") {
                         continue;
                     }
-                    let Some((value, suffix)) = value.split_once(' ') else {
+                    let mut parts = value.splitn(2, ' ');
+                    let Ok(nanos) = parts.next().unwrap_or_default().parse::<u64>() else {
                         continue;
                     };
-                    println!("  engine {}: {} {}", key, value, suffix);
+                    match parts.next().unwrap_or_default() {
+                        "ns" => {
+                            // Nanoseconds
+                        }
+                        // Other suffixes not defined
+                        _ => {
+                            continue;
+                        }
+                    }
+                    println!("  engine {}: {:?}", key, Duration::from_nanos(nanos));
                 }
             }
         }
     }
-    eprintln!("Collected in {:?}", elapsed);
+
+    eprintln!(
+        "Collected in {:?}, printed in {:?}",
+        elapsed,
+        instant.elapsed()
+    );
 }
